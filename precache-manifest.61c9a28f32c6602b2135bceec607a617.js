b1a95e202fc0a8fab399c76ed175fcd8ed5dff42
gusleaooliveira <gus.leaono@gmail.com>
2020-06-29 21:28:49 -0300

    alteração feita em:29/06/2020-21:28:49

---
+++ precache-manifest.61c9a28f32c6602b2135bceec607a617.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b04954bbe6380750c875604c6f54c2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47b9afac14b96b92b884",
+    "url": "/static/css/main.db8352bf.chunk.css"
+  },
+  {
+    "revision": "c0c3094071dffccfac12",
+    "url": "/static/js/2.87bb0fda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.87bb0fda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b9afac14b96b92b884",
+    "url": "/static/js/main.014bf622.chunk.js"
+  },
+  {
+    "revision": "08f539074c6166439b66",
+    "url": "/static/js/runtime-main.e5d6a247.js"
+  }
+]);
